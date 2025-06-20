e073507b7075b9017185bd10e202d76a26a516ea
Brian Worthington <worthington.brian@realtyts.com>
2025-06-20 12:02:58 -0500

    Switch to static website deployment approach

--- deploy.cmd
+++ deploy.cmd
@@ -1,47 +1,25 @@
-@echo off
+@if "%SCM_TRACE_LEVEL%" NEQ "4" @echo off
 
-echo Starting deployment...
+:: ----------------------
+:: KUDU Deployment Script for Static Website
+:: ----------------------
 
 :: Setup
 setlocal enabledelayedexpansion
 
-:: 1. KuduSync - Copy all files from source to target
-echo Syncing all files to deployment target...
-call :ExecuteCmd "%KUDU_SYNC_CMD%" -v 50 -f "%DEPLOYMENT_SOURCE%" -t "%DEPLOYMENT_TARGET%" -n "%NEXT_MANIFEST_PATH%" -p "%PREVIOUS_MANIFEST_PATH%" -i ".git;.hg;.deployment;deploy.cmd"
+:: 1. KuduSync - Copy build files from source to target
+echo Syncing build files to deployment target...
+call :ExecuteCmd "%KUDU_SYNC_CMD%" -v 50 -f "%DEPLOYMENT_SOURCE%\build" -t "%DEPLOYMENT_TARGET%" -n "%NEXT_MANIFEST_PATH%" -p "%PREVIOUS_MANIFEST_PATH%" -i ".git;.hg;.deployment;deploy.cmd;node_modules"
 IF !ERRORLEVEL! NEQ 0 goto error
 
-:: 2. Copy server.js to deployment target
-echo Copying server.js to deployment target...
-copy "%DEPLOYMENT_SOURCE%\server.js" "%DEPLOYMENT_TARGET%\server.js"
-IF !ERRORLEVEL! NEQ 0 goto error
-
-:: 3. Copy web.config to deployment target
+:: 2. Copy web.config to deployment target
 echo Copying web.config to deployment target...
-copy "%DEPLOYMENT_SOURCE%\web.config" "%DEPLOYMENT_TARGET%\web.config"
+call :ExecuteCmd copy "%DEPLOYMENT_SOURCE%\web.config" "%DEPLOYMENT_TARGET%\web.config"
 IF !ERRORLEVEL! NEQ 0 goto error
 
-:: 4. Copy package.json to deployment target
-echo Copying package.json to deployment target...
-copy "%DEPLOYMENT_SOURCE%\package.json" "%DEPLOYMENT_TARGET%\package.json"
-IF !ERRORLEVEL! NEQ 0 goto error
-
-:: 5. Install only express and path dependencies
-echo Installing minimal server dependencies...
-pushd "%DEPLOYMENT_TARGET%"
-call npm install express path --no-optional
-IF !ERRORLEVEL! NEQ 0 goto error
-popd
-
-echo Deployment completed successfully.
+:: Finished successfully
+echo Static website deployment completed successfully.
 goto end
-
-:ExecuteCmd
-setlocal
-set _CMD_=%*
-call %_CMD_%
-if "%ERRORLEVEL%" NEQ "0" echo Failed executing %_CMD_% && exit /b 1
-endlocal
-exit /b 0
 
 :error
 echo An error occurred during deployment.
@@ -49,3 +27,13 @@ exit /b 1
 
 :end
 echo Finished successfully.
+exit /b 0
+
+:: Execute command routine that will echo out when error
+:ExecuteCmd
+setlocal
+set _CMD_=%*
+call %_CMD_%
+if "%ERRORLEVEL%" NEQ "0" echo Failed executing '%_CMD_%' && exit /b 1
+endlocal
+goto :EOF
