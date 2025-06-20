7cd583f2b9dc7acb96eb81d6c97fb8017a355da5
Brian Worthington <worthington.brian@realtyts.com>
2025-06-20 08:46:48 -0500

    Add server.js and configure for Azure Windows deployment

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+
+// Serve static files from the React build
+app.use(express.static(path.join(__dirname, 'build')));
+
+// Handle requests to any route by serving the index.html
+app.get('*', function (req, res) {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
+
+const port = process.env.PORT || 8080;
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
