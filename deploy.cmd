6bd5ec689d9cbe41ae3ee402699cdab957164156
Brian Worthington <worthington.brian@realtyts.com>
2025-06-20 10:40:01 -0500

    Switch to pre-built deployment approach to fix Node.js version issues

--- deploy.cmd
+++ deploy.cmd
@@ -1,42 +1,50 @@
 @echo off
-echo Starting deployment script...
+
+echo Starting deployment...
 
 :: Setup
 setlocal enabledelayedexpansion
 
-:: 1. KuduSync
-IF /I "%IN_PLACE_DEPLOYMENT%" NEQ "1" (
-  echo Running KuduSync...
-  call :ExecuteCmd "%KUDU_SYNC_CMD%" -v 50 -f "%DEPLOYMENT_SOURCE%" -t "%DEPLOYMENT_TARGET%" -n "%NEXT_MANIFEST_PATH%" -p "%PREVIOUS_MANIFEST_PATH%" -i ".git;.hg;.deployment;deploy.cmd"
-  IF !ERRORLEVEL! NEQ 0 goto error
-)
+:: 1. KuduSync - Copy pre-built files from build directory
+echo Syncing pre-built files to deployment target...
+call :ExecuteCmd "%KUDU_SYNC_CMD%" -v 50 -f "%DEPLOYMENT_SOURCE%\build" -t "%DEPLOYMENT_TARGET%" -n "%NEXT_MANIFEST_PATH%" -p "%PREVIOUS_MANIFEST_PATH%" -i ".git;.hg;.deployment;deploy.cmd;node_modules"
+IF !ERRORLEVEL! NEQ 0 goto error
 
-:: 2. Install npm packages
-IF EXIST "%DEPLOYMENT_TARGET%\package.json" (
-  echo Installing npm packages...
-  pushd "%DEPLOYMENT_TARGET%"
-  
-  :: Install dependencies
-  call npm install
-  IF !ERRORLEVEL! NEQ 0 goto error
-  
-  :: Build the app
-  call npm run build
-  IF !ERRORLEVEL! NEQ 0 goto error
-  
-  popd
-)
+:: 2. Copy server.js to deployment target
+echo Copying server.js to deployment target...
+copy "%DEPLOYMENT_SOURCE%\server.js" "%DEPLOYMENT_TARGET%\server.js"
+IF !ERRORLEVEL! NEQ 0 goto error
 
+:: 3. Copy web.config to deployment target
+echo Copying web.config to deployment target...
+copy "%DEPLOYMENT_SOURCE%\web.config" "%DEPLOYMENT_TARGET%\web.config"
+IF !ERRORLEVEL! NEQ 0 goto error
+
+:: 4. Copy package.json to deployment target
+echo Copying package.json to deployment target...
+copy "%DEPLOYMENT_SOURCE%\package.json" "%DEPLOYMENT_TARGET%\package.json"
+IF !ERRORLEVEL! NEQ 0 goto error
+
+:: 5. Install only production dependencies
+echo Installing production dependencies...
+pushd "%DEPLOYMENT_TARGET%"
+call npm install --only=production --no-optional
+IF !ERRORLEVEL! NEQ 0 goto error
+popd
+
+echo Deployment completed successfully.
 goto end
 
 :ExecuteCmd
 setlocal
 set _CMD_=%*
 call %_CMD_%
-exit /b %ERRORLEVEL%
+if "%ERRORLEVEL%" NEQ "0" echo Failed executing %_CMD_% && exit /b 1
+endlocal
+exit /b 0
 
 :error
-echo An error has occurred during web site deployment.
+echo An error occurred during deployment.
 exit /b 1
 
 :end
